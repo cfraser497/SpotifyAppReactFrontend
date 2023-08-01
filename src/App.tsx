0e286c1300663afb9ee12949161dc7b1a4a0ec9f
Cameron Fraser <cfraser497@gmail.com>
2023-08-01 01:30:12 +0100

    [Feature] Can filter by modality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import SearchIcon from "./assets/search.svg";
-import './App.css';
+import "./App.css";
 import { extractPlaylistId } from "./utils";
 import Track from "./components/songList/Track";
 import FilterSongs from "./components/songFilter/FilterSongs";
@@ -9,9 +9,10 @@ import FilterSongs from "./components/songFilter/FilterSongs";
 function App() {
   const [playlistLink, setPlaylistLink] = useState("");
   const [tracks, setTracks] = useState([]);
-  const [filterValue, setFilterValue] = useState("-1");
+  const [filterKeyValue, setFilterKeyValue] = useState("-1");
+  const [filterModeValue, setFilterModeValue] = useState("-1");
 
-  const getSongs = async (e: { preventDefault: () => void; }) => {
+  const getSongs = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     await axios
       .post("http://localhost:3000/", {
@@ -25,28 +26,55 @@ function App() {
       });
   };
 
-  function onFilterValueSelected(filterValue: string) {
-    setFilterValue(filterValue);
+  function onFilterModeValueSelected(filterModeValue: string) {
+    setFilterModeValue(filterModeValue);
+  }
+
+  function onFilterValueSelected(filterKeyValue: string) {
+    setFilterKeyValue(filterKeyValue);
   }
 
   let filteredTracks = tracks.filter((track) => {
-    switch(filterValue) {
-      case "-1": return true;
-      case "0": return track.key === 0;
-      case "1": return track.key === 1;
-      case "2": return track.key === 2;
-      case "3": return track.key === 3;
-      case "4": return track.key === 4;
-      case "5": return track.key === 5;
-      case "6": return track.key === 6;
-      case "7": return track.key === 7;
-      case "8": return track.key === 8;
-      case "9": return track.key === 9;
-      case "10": return track.key === 10;
-      case "11": return track.key === 11;
+    switch (filterKeyValue) {
+      case "-1":
+        return true;
+      case "0":
+        return track.key === 0;
+      case "1":
+        return track.key === 1;
+      case "2":
+        return track.key === 2;
+      case "3":
+        return track.key === 3;
+      case "4":
+        return track.key === 4;
+      case "5":
+        return track.key === 5;
+      case "6":
+        return track.key === 6;
+      case "7":
+        return track.key === 7;
+      case "8":
+        return track.key === 8;
+      case "9":
+        return track.key === 9;
+      case "10":
+        return track.key === 10;
+      case "11":
+        return track.key === 11;
     }
     return false;
-  })
+  }).filter((track) => {
+    switch(filterModeValue) {
+      case "-1":
+        return true;
+      case "0":
+        return track.mode === 0;
+      case "1":
+        return track.mode === 1;
+    }
+    return false;
+  });
 
   return (
     <div className="app">
@@ -59,9 +87,11 @@ function App() {
         />
         <img src={SearchIcon} alt="search" onClick={getSongs} />
       </div>
-      <FilterSongs filterValueSelected={onFilterValueSelected}></FilterSongs>
+      <FilterSongs filterKeyValueSelected={onFilterValueSelected} filterModeValueSelected={onFilterModeValueSelected}></FilterSongs>
       <ul id="songlist">
-          {filteredTracks.map((track) => (<Track track = {track} />))}
+        {filteredTracks.map((track) => (
+          <Track track={track} />
+        ))}
       </ul>
     </div>
   );
--- src/components/songFilter/FilterSongs.tsx
+++ src/components/songFilter/FilterSongs.tsx
@@ -1,13 +1,17 @@
 import './FilterSongs.css'
 
 function FilterSongs (props: any) {
-    function onFilterValueChanged(event: any) {
-        props.filterValueSelected(event.target.value);
+    function onKeyValueChanged(event: any) {
+        props.filterKeyValueSelected(event.target.value);
+    }
+
+    function onModeValueChanged(event) {
+        props.filterModeValueSelected(event.target.value);
     }
 
     return (<div className="filterArea">
         <label className="keyLabel">Key: </label>
-        <select name="filterKey" onChange={onFilterValueChanged}>
+        <select className = "keyDropdown" name="filterKey" onChange={onKeyValueChanged}>
             <option value="-1">--</option>
             <option value="0">C</option>
             <option value="1">C#</option>
@@ -21,6 +25,12 @@ function FilterSongs (props: any) {
             <option value="9">A</option>
             <option value="10">A#</option>
             <option value="11">B</option>
+        </select>
+        <label className="keyLabel">Modality: </label>
+        <select className = "keyDropdown" name="filterMode" onChange={onModeValueChanged}>
+            <option value="-1">--</option>
+            <option value="0">Minor</option>
+            <option value="1">Major</option>
         </select>
     </div>);
 }
