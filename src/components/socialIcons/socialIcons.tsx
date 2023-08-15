6198ce61abaeef55172b4deb7a43d0a0c4add820
Cameron Fraser <cfraser497@gmail.com>
2023-08-15 18:42:36 +0100

    [Style] changed implemenation of social icon hover effect

--- src/components/socialIcons/socialIcons.tsx
+++ src/components/socialIcons/socialIcons.tsx
@@ -4,25 +4,25 @@ import "./socialIcons.css";
 function SocialIcons() {
   return (
     <div className="socialIconsBox">
-      <SocialIcon
+      <SocialIcon className="socialIcon"
         url="https://open.spotify.com/artist/2tib6uzGOLCaPCDLW5Dzno?si=DPeVVSVJS-mk3K7Cx-lykA"
         target="_blank"
         rel="noopener noreferrer"
       />
-      <SocialIcon
+      <SocialIcon className="socialIcon"
         url="https://www.youtube.com/@anotherworld8863/"
         bgColor="#eeeeee"
         fgColor="#F81112"
         target="_blank"
         rel="noopener noreferrer"
       />
-      <SocialIcon
+      <SocialIcon className="socialIcon"
         url="https://www.instagram.com/another_world_music/"
         fgColor="#eeeeee"
         target="_blank"
         rel="noopener noreferrer"
       />
-      <SocialIcon
+      <SocialIcon className="socialIcon"
         url="https://www.tiktok.com/@iamanotherworldmusic"
         fgColor="#eeeeee"
         target="_blank"
