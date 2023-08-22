f61f8353d8f59d6f142aeff3a63faf7106bb2187
Cameron Fraser <cfraser497@gmail.com>
2023-08-23 00:08:52 +0100

    [Feature] Added key double checking

--- src/components/recommendedList/RecommendedSongs.tsx
+++ src/components/recommendedList/RecommendedSongs.tsx
@@ -3,6 +3,7 @@ import "./RecommendedSongs.css";
 
 const maxScore = 100;
 const keyWeight = 4;
+const maxKeyScore = 50;
 const tempoWeight = 1;
 const genreWeight = 5;
 const energyWeight = 2;
@@ -13,7 +14,6 @@ const keyDiff = 2;
 const diffModePenalty = 30;
 
 function RecommendedSongs(props: any) {
-
   //returns the number of common genres between two ARTISTS (spotify API doesnt have genre information on songs)
   function calculateGenreScore(track1, track2): number {
     const track1Genres = track1.genres;
@@ -33,16 +33,40 @@ function RecommendedSongs(props: any) {
   }
 
   function calculateScore(
-    track1: { name: string; tempo: number; key: number; genres: string[]; mode: number, energy: number },
-    track2: { name: string; tempo: number; key: number; genres: string[]; mode: number, energy: number }
+    track1: {
+      name: string;
+      tempo: number;
+      key: number;
+      genres: string[];
+      mode: number;
+      energy: number;
+    },
+    track2: {
+      name: string;
+      tempo: number;
+      key: number;
+      genres: string[];
+      mode: number;
+      energy: number;
+    }
   ): number {
-    const tempoScore = tempoWeight * Math.abs(track1.tempo - track2.tempo);
-    const keyScore = keyWeight ** Math.abs(track1.key - track2.key) - 1; //0 -> 0; 1 -> keyweight - 1; 2 -> keyweight^2 - 1
+    const tempoScore =
+      tempoWeight *
+      Math.min(
+        Math.abs(track1.tempo - track2.tempo),
+        Math.abs(track1.tempo * 2 - track2.tempo),
+        Math.abs(track1.tempo - 2 * track2.tempo)
+      );
+    const keyScore = Math.min(maxKeyScore, keyWeight ** Math.abs(track1.key - track2.key) - 1); //0 -> 0; 1 -> keyweight - 1; 2 -> keyweight^2 - 1
     const modeScore = track1.mode == track2.mode ? 0 : diffModePenalty;
     const genreScore = calculateGenreScore(track1, track2);
-    const energyScore = Math.round(energyWeight * 10 * Math.abs(track1.energy - track2.energy));
+    const energyScore = Math.round(
+      energyWeight * 10 * Math.abs(track1.energy - track2.energy)
+    );
 
-    return maxScore - tempoScore - keyScore - genreScore - modeScore - energyScore;
+    return (
+      maxScore - tempoScore - keyScore - genreScore - modeScore - energyScore
+    );
   }
 
   function createRecommendations(filteredTracks: any[], checkedTrack: any) {
@@ -56,7 +80,9 @@ function RecommendedSongs(props: any) {
           let track2 = filteredTracks[j];
           if (
             Math.abs(track1.key - track2.key) <= keyDiff &&
-            Math.abs(track1.tempo - track2.tempo) <= tempoDiff &&
+            (Math.abs(track1.tempo - track2.tempo) <= tempoDiff ||
+              Math.abs(track1.tempo * 2 - track2.tempo) <= tempoDiff ||
+              Math.abs(track1.tempo - track2.tempo * 2) <= tempoDiff) &&
             track1.mode == track2.mode
           ) {
             let score = calculateScore(track1, track2);
@@ -71,7 +97,11 @@ function RecommendedSongs(props: any) {
     } else {
       for (let i = 0; i < filteredTracks.length; i++) {
         let track1 = filteredTracks[i];
-        if (track1.id != checkedTrack.id && track1.tempo && checkedTrack.tempo) {
+        if (
+          track1.id != checkedTrack.id &&
+          track1.tempo &&
+          checkedTrack.tempo
+        ) {
           let score = calculateScore(track1, checkedTrack);
           recommendations.push({
             track1: checkedTrack,
